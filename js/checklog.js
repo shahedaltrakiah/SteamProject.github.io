6fda9d5d6af5db648c55152cc8daf281df9f27f1
ajarmeh-oroub <ajarmehoroub12@gmail.com>
2024-10-05 02:59:28 +0300

    Update checklog.js

--- js/checklog.js
+++ js/checklog.js
@@ -1,35 +1,32 @@
 
 
-
-// Function to check if the user is logged in
 function checkUserLogin() {
     const email = getQueryParam('email'); 
     let userData;
 
-   
     if (email) {
         userData = JSON.parse(localStorage.getItem(email));
     }
 
-   
     if (userData && userData.logged === "Yes") {
-       
         var logoutButton = document.querySelector('.btn-custom-1'); 
-        logoutButton.innerText = "Logout"; 
-        logoutButton.onclick = logout;
+        if (logoutButton) {
+            logoutButton.innerText = "Logout";
+            logoutButton.onclick = logout;
+        }
 
-       
-        var icon = document.querySelector('.for-logging'); 
-        icon.innerHTML = `<a href="pages/profile.html?email=${encodeURIComponent(email)}"><i class="fa-solid fa-user"></i></a>`;
-      
+        var icon = document.querySelector('.for-logging');
+        if (icon) {
+            icon.innerHTML = `<a href="pages/profile.html?email=${encodeURIComponent(email)}"><i class="fa-solid fa-user"></i></a>`;
+        }
     } else {
-        // User is not logged in
         var loginButton = document.querySelector('.btn-custom-1'); 
-        loginButton.innerText = "Login";
-        // loginButton.onclick = login;
+        if (loginButton) {
+            loginButton.innerText = "Login";
+            // loginButton.onclick = login; // Make sure this function exists
+        }
     }
 }
-
 
 function logout() {
     const email = getQueryParam('email'); 
@@ -39,17 +36,15 @@ function logout() {
             userData.logged = "no"; 
             localStorage.setItem(email, JSON.stringify(userData)); 
             console.log("User logged out.");
-            checkUserLogin();
-            window.location.href = "../index.html"; 
+            window.location.href = "../index.html"; // Redirect after logging out
         }
     }
 }
 
-
 window.onload = checkUserLogin;
-
 
 function getQueryParam(name) {
     const urlParams = new URLSearchParams(window.location.search);
     return urlParams.get(name); 
 }
+
